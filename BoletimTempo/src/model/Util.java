48b1d3b8d5c45005dd14edfe18dd549eaabeeca3
Patrick M Lima <Patrick M Lima@Orion>
2015-01-09 03:22:53 -0400

    Adição de elementos na classe Util

--- BoletimTempo/src/model/Util.java
+++ BoletimTempo/src/model/Util.java
@@ -11,6 +11,10 @@ import java.io.File;
  */
 public class Util {
 	public static String SEPARATOR = File.separator;
-	public static String OUTPUT_FOLDER = "."+Util.SEPARATOR+"src"+Util.SEPARATOR+"data" + Util.SEPARATOR;
-	public static String BAIXA1 = "."+Util.SEPARATOR+"src"+Util.SEPARATOR+"data"+Util.SEPARATOR + "CR3000_Estacao_Baixa1.dat";
+	public static String OUTPUT_FOLDER = "." + Util.SEPARATOR + "src" + Util.SEPARATOR + "data" + Util.SEPARATOR;
+	public static String BAIXA1 = OUTPUT_FOLDER + "CR3000_Estacao_Baixa1.dat";
+	public static String IM_SUN = OUTPUT_FOLDER + "img" + Util.SEPARATOR + "img_sun.png";
+	public static String IM_RAIN = OUTPUT_FOLDER + "img" + Util.SEPARATOR + "img_rain.png";
+	public static String IM_WIND = OUTPUT_FOLDER + "img" + Util.SEPARATOR + "img_wind.png";
+	public static String IM_THERMO = OUTPUT_FOLDER + "img" + Util.SEPARATOR + "img_thermometer.png";
 }
