149ddbfbf5bafde1b12bca7f0fae3b45e5906160
Patrick Lima <patrickm.lima1@gmail.com>
2015-02-04 01:00:50 -0400

    Classe que implementa a obtenção dos elementos de um Array de forma circular

---
+++ BoletimTempo/src/model/util/CircularArrayList.java
@@ -0,0 +1,61 @@
+package model.util;
+
+import java.util.ArrayList;
+
+/**
+ * Class which can be treated as a circular array
+ * @author Patrick M Lima
+ * 
+ * @param <E> data type of objects which will be added to array
+ */
+public class CircularArrayList<E> extends ArrayList<E> {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int index;
+	
+	/**
+	 * Default class constructor
+	 */
+	public CircularArrayList() {
+		super();
+		index = -1;
+	}
+	
+	/**
+	 * Takes the next array element in sequential form.
+	 * If is the last element, returns the first array element.
+	 * @return an array element
+	 */
+	public E getNext() {
+		if(!super.isEmpty()) {
+			if(index >= this.size()-1)
+				index = -1;
+			return this.get(++index);
+		}
+		return null;
+	}
+	
+	/**
+	 * Takes the previous array element in sequential form.
+	 * If is the first element, returns the last array element.
+	 * @return an array element
+	 */
+	public E getPrevious() {
+		if(!super.isEmpty()) {
+			if(index <= 0)
+				index = this.size();
+			return this.get(--index);
+		}
+		return null;
+	}
+	
+	@Override
+	public void clear() {
+		super.clear();
+		index = -1;
+	}
+	
+	
+}
