ab359b382b08e0edc7211e51b5085d54d1ada905
Patrick Lima <patrickm.lima1@gmail.com>
2015-02-11 02:04:39 -0400

    alteração em controller

--- BoletimTempo/src/controller/Controller.java
+++ BoletimTempo/src/controller/Controller.java
@@ -6,7 +6,7 @@ import model.process.WeatherDayInterface;
 import model.util.DateUtil;
 
 /**
- * It's the means to access the weather report 
+ * It's the means to access the weather report
  * 
  * @author Elloa
  *
@@ -18,24 +18,26 @@ public class Controller {
 
 	/**
 	 * 
-	 * @throws IOException if the file can't be accessed or doesn't exist
+	 * @throws IOException
+	 *             if the file can't be accessed or doesn't exist
 	 */
-	private Controller() throws IOException{
+	private Controller() throws IOException {
 		wdInterface = new WeatherDayInterface();
 	}
-	
+
 	/**
 	 * returns the Controller instance (only one instance can exist)
+	 * 
 	 * @return the Controller instance reference
 	 * @throws IOException
 	 */
-	public static Controller getInstance() throws IOException{
-		if (instance == null){
+	public static Controller getInstance() throws IOException {
+		if (instance == null) {
 			instance = new Controller();
 		}
 		return instance;
 	}
-	
+
 	/**
 	 * Communicates with WeatherDay instance and computes all days
 	 */
@@ -44,36 +46,45 @@ public class Controller {
 		wdInterface.saveDays();
 		wdInterface.clearDays();
 	}
-	
+
 	/**
 	 * Communicates with WeatherDay instance and computes only a day
-	 * @param day the day to be processed 
+	 * 
+	 * @param day
+	 *            the day to be processed
 	 */
-	public void computeWeatherDay(String day) {
-		wdInterface.readADay(day);
-		wdInterface.saveDays();
+	public boolean computeWeatherDay(String day) {
+		if(wdInterface.readADay(day)) {
+			saveAndClear();
+			return true;
+		}
 		wdInterface.clearDays();
+		return false;
 	}
-	
+
 	/**
 	 * Communicates with WeatherDay instance and computes a range of days
-	 * @param initialDay the day to begins the processing
-	 * @param finalDay the day to finalize the processing
+	 * 
+	 * @param initialDay
+	 *            the day to begins the processing
+	 * @param finalDay
+	 *            the day to finalize the processing
 	 */
-	public void computeWeatherDay(String initialDay, String finalDay) {
-		wdInterface.readRangeDays(initialDay, finalDay);
+	public boolean computeWeatherDay(String initialDay, String finalDay) {
+		if(wdInterface.readRangeDays(initialDay, finalDay)) {
+			saveAndClear();
+			return true;
+		}
+		wdInterface.clearDays();
+		return false;
+	}
+
+	public boolean validateDate(String firstDate, String lastDate) {
+		return DateUtil.isChronological(firstDate, lastDate);
+	}
+	
+	private void saveAndClear() {
 		wdInterface.saveDays();
 		wdInterface.clearDays();
-	}
-	
-	public boolean validateDate(String date) {
-		return DateUtil.isValidDate(date);
-	}
-	
-	public boolean validateDate(String firstDate, String lastDate) {
-		if(validateDate(firstDate) && validateDate(lastDate)) {
-			return DateUtil.isChronological(firstDate, lastDate);
-		}
-		return false;
 	}
 }
