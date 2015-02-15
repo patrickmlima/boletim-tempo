21b063f231a23dafea0f069efdb9113f963b3c4d
Patrick Lima <patrickm.lima1@gmail.com>
2015-02-15 14:47:03 -0400

    melhoria na escolha das opções

--- BoletimTempo/src/model/actionlistener/ProcessDataBtnHandler.java
+++ BoletimTempo/src/model/actionlistener/ProcessDataBtnHandler.java
@@ -6,15 +6,21 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 
 import javax.swing.JButton;
-import javax.swing.JTabbedPane;
 
+import model.util.ApplicationStatus;
 import view.ProcessDataView;
 import view.WorkWindow;
 import view.popup.DialogBox;
 import controller.Controller;
 
-public class ProcessBtnHandler implements ActionListener {
-	public ProcessBtnHandler() {
+/**
+ * Handle of Process Button within ProcessDataView class
+ * @author Patrick M Lima
+ *
+ */
+public class ProcessDataBtnHandler implements ActionListener {
+	
+	public ProcessDataBtnHandler() {
 	}
 
 	@Override
@@ -26,82 +32,119 @@ public class ProcessBtnHandler implements ActionListener {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		if(WorkWindow.getInstance().getStatus() != ApplicationStatus.INITIALIZED) {
+			controller.clearWeatherDay();
+			WorkWindow.getInstance().configureTabbedPaneEnable();
+		}
+		
 		// pega o JPanel ProcessDataView
 		ProcessDataView processDataView = (ProcessDataView) ((JButton) source
 				.getSource()).getParent().getParent();
-		DialogBox dialog = new DialogBox();
-		WorkWindow workWindow = WorkWindow.getInstance();
 		
-		//se o radiobutton de todos os dias está selecionado
-		if (processDataView.isAllDaysSelected()) {
-			//computa e mostra uma mensagem ao fim
+		WorkWindow workWindow = WorkWindow.getInstance();
+		//pega a opção da janela, e de acordo com ela, toma as decisões
+		ProcessDataViewCases result = ranksTheCase(processDataView);
+		
+		switch(result) {
+		case ALL_DAY_SELECTED:
 			workWindow.setNotClosable();
 			controller.computeWeatherDay();
-			
-			dialog.initialize("Processamento finalizado",
+
+			(new DialogBox()).initialize("Processamento finalizado",
 					"Todos os dias foram processados\n e salvos com sucesso.",
 					processDataView, "OK");
 			changeTabbed(workWindow);
 			workWindow.setClosable();
-			
-		//se o radiobutton de um dia está selecionado
-		} else if (processDataView.isADaySelected()) {
-			workWindow.setNotClosable();
-			//verifica se o dia foi computado
+			break;
+
+		case ONE_DAY_SELECTED:
+			//se o retorno da computação é true (computou o dia)
 			if (controller.computeWeatherDay(new SimpleDateFormat("dd/MM/yyyy").
-					format(processDataView.getDateChooserADay().getDate()))) {
-				//se sim, exibe uma mensagem de finalização
-				dialog.initialize("Processamento finalizado",
+					format(processDataView.getDateChooserADay().getDate())) ) {
+				//exibe uma mensagem de finalização
+				(new DialogBox()).initialize("Processamento finalizado",
 						"Todos os dias foram processados\n e salvos com sucesso.",
 						processDataView, "OK");
 				changeTabbed(workWindow);
 			}
 			//senão, exibe uma mensagem informando que o dia não foi processado
 			else {
-				dialog.initialize("Processamento não realizado",
+				(new DialogBox()).initialize("Processamento não realizado",
 						"O dia não é válido ou ainda\n não foi salvo no arquivo de dados.",
 						processDataView, "error");
 			}
 			workWindow.setClosable();
-		//se o radiobutton de um conjunto de dias está selecionado
-		} else if (processDataView.isRangeDaysSelected()) {
+			break;
+
+		case RANGE_OF_DAYS_SELECTED:
 			SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
 			String fDay, lDay;
 			fDay = formatter.format(processDataView.getDateChooserFirstDay()
 					.getDate());
 			lDay = formatter.format(processDataView.getDateChooserLastDay()
 					.getDate());
+
 			//verifica se as datas estão em ordem cronológica
 			if (controller.validateDate(fDay, lDay)) {
 				workWindow.setNotClosable();
+
 				//verifica se os dias foram computados
-				if (controller.computeWeatherDay(fDay, lDay)) {
+				if (controller.computeWeatherDay(fDay, lDay) ) {
 					//se sim, exibe uma mensagem informando o fim do processamento
-					dialog.initialize("Processamento finalizado",
+					(new DialogBox()).initialize("Processamento finalizado",
 							"Todos os dias foram processados\n e salvos com sucesso.",
 							processDataView, "OK");
+
 					changeTabbed(workWindow);
 				}
 				//senão, exibe uma mesagem informando o não processamento
 				else {
-					dialog.initialize("Processamento não realizado",
+					(new DialogBox()).initialize("Processamento não realizado",
 							"O dia não é válido ou ainda\n não foi salvo no arquivo de dados.",
 							processDataView, "error");
 				}
 				workWindow.setClosable();
-			//se as datas não são cronológicas exibe uma mensagem informando o fato
+
+				//se as datas não são cronológicas exibe uma mensagem informando o fato
 			} else {
-				dialog.initialize("Processamento não realizado",
+				(new DialogBox()).initialize("Processamento não realizado",
 						"As datas precisam estar em\n ordem cronológia.",
 						processDataView, "incorrect");
 			}
-		} else {
-			dialog.initialize("Por favor", "Selecione uma opção para iniciar o processamento", processDataView, "incorrect");
+			break;
+
+		case NO_DATE_SELECTED:
+			(new DialogBox()).initialize("Processamento não realizado",
+					"Selecione uma data para iniciar o processamento",
+					processDataView, "incorrect");
+			break;
+
+		case NO_OPTIONS_SELECTED:
+			(new DialogBox()).initialize("Por favor", "Selecione uma opção para iniciar o processamento", processDataView, "incorrect");
+			break;
 		}
-	}
+	}		
 	
 	private void changeTabbed(WorkWindow workWindow) {
 		workWindow.setSelectedTab(1);
 //		workWindow.configureTabbedPaneEnable(tabbedPane);
 	}
+	
+	private ProcessDataViewCases ranksTheCase(ProcessDataView processDataView) {
+		if (processDataView.isAllDaysSelected()) {
+			return ProcessDataViewCases.ALL_DAY_SELECTED;
+		} else if (processDataView.isADaySelected()) {
+			if(processDataView.getDateChooserADay().getDate() == null)
+				return ProcessDataViewCases.NO_DATE_SELECTED;
+			return ProcessDataViewCases.ONE_DAY_SELECTED;
+		} else if (processDataView.isRangeDaysSelected()) {
+			if(processDataView.getDateChooserFirstDay().getDate() == null || processDataView.getDateChooserLastDay().getDate() == null)
+				return ProcessDataViewCases.NO_DATE_SELECTED;
+			return ProcessDataViewCases.RANGE_OF_DAYS_SELECTED;
+		} else {
+			return ProcessDataViewCases.NO_OPTIONS_SELECTED;
+		}
+
+	}
+
 }
