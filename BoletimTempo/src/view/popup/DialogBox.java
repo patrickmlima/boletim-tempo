f6b7b136785dbe82f4a62d1701ea7201733c42c8
Patrick Lima <patrickm.lima1@gmail.com>
2015-02-15 14:57:29 -0400

    caixa de diálogo que mostra os avisos da aplicação

--- BoletimTempo/src/view/popup/DialogBox.java
+++ BoletimTempo/src/view/popup/DialogBox.java
@@ -1,17 +1,25 @@
 package view.popup;
 
 import java.awt.Component;
+import java.awt.Font;
 
 import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.plaf.FontUIResource;
 
 public class DialogBox {
 	public DialogBox() {
 	}
 	
-	public void initialize(String title, String text, Component parent, String type) {
+	public void initialize(String title, String text, Component parent, String type) {		
+		Font fontPopup = new Font("Cambria Math", Font.PLAIN, 18);
+		UIManager.put("OptionPane.messageFont", new FontUIResource(fontPopup));
+		UIManager.put("OptionPane.buttonFont", new FontUIResource(fontPopup));
+		UIManager.put("OptionPane.titleFont", new FontUIResource(new Font("Cambria Math", Font.BOLD, 20)));
 		if(type.equals("OK")) {
 			JOptionPane.setDefaultLocale(null);
 			JOptionPane.showMessageDialog(parent, text, title, JOptionPane.INFORMATION_MESSAGE);
+			
 		} else if(type.equals("incorrect")) {
 			JOptionPane.setDefaultLocale(null);
 			JOptionPane.showMessageDialog(parent, text, title, JOptionPane.WARNING_MESSAGE);
