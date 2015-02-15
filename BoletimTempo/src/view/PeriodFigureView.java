90533e4de9f2ec3ed3fc48dabd9a1ecec648851d
Patrick Lima <patrickm.lima1@gmail.com>
2015-02-15 14:56:41 -0400

    View para visualizar e salvar as period figures (opção de salvar já funcionando)

--- BoletimTempo/src/view/PeriodFigureView.java
+++ BoletimTempo/src/view/PeriodFigureView.java
@@ -3,19 +3,15 @@ package view;
 import javax.swing.JPanel;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
-import javax.swing.JFormattedTextField;
 
 import java.awt.Font;
-import java.awt.Image;
-import java.text.ParseException;
 
-import model.actionlistener.ShowFigureHandler;
-import model.util.CircularArrayList;
+import model.actionlistener.ShowFigureBtnsHandler;
+import model.componetchangelistener.PeriodFigureViewChanges;
 
 import javax.swing.JButton;
 import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.text.MaskFormatter;
-import javax.swing.JLabel;
+
 import java.awt.Color;
 
 public class PeriodFigureView extends JPanel {
@@ -24,38 +20,51 @@ public class PeriodFigureView extends JPanel {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	private JPanel panelShowFigures;
 
 	/**
 	 * Create the panel.
 	 */
 	public PeriodFigureView() {
+		addComponentListener(new PeriodFigureViewChanges());
 		initialize();
 	}
 	
+	public void setPanelShowFigure(JPanel panelShowFigures) {
+		this.panelShowFigures = panelShowFigures;
+	}
+	
+	public JPanel getPanelShowFigures() {
+		return panelShowFigures;
+	}
+	
 	public void initialize() {
-		MaskFormatter maskDate = null;
-		try {
-			maskDate = new MaskFormatter("##/##/####");
-			maskDate.setPlaceholderCharacter('_');
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
+		//Create the panel to show the figures
+		panelShowFigures = new JPanel();
+		panelShowFigures.setBackground(Color.GRAY);
 		
+		//Cria o JButton 'Anterior'
 		JButton buttonNext = new JButton("Anterior");
 		buttonNext.setToolTipText("Clique para ver as figuras do dia anterior");
 		buttonNext.setFont(new Font("Cambria Math", Font.PLAIN, 18));
 		
-		JButton btnSaveFigures = new JButton("Salvar figuras");
-		btnSaveFigures.setToolTipText("Clique aqui para salvar as figuras do turno");
-		btnSaveFigures.setFont(new Font("Cambria Math", Font.PLAIN, 18));
-		
-		JPanel panelShowFigures = new JPanel();
-		panelShowFigures.setBackground(Color.GRAY);
-		
+		//Cria JButton 'Próximo'
 		JButton buttonPrevious = new JButton("Pr\u00F3ximo");
 		buttonPrevious.setToolTipText("Clique para ver as figuras do pr\u00F3ximo dia");
 		buttonPrevious.setFont(new Font("Cambria Math", Font.PLAIN, 18));
 		
+		//Cria o JButton 'Salvar figuras'
+		JButton btnSaveFigures = new JButton("Salvar figuras");
+		btnSaveFigures.setToolTipText("Clique aqui para salvar as figuras do turno");
+		btnSaveFigures.setFont(new Font("Cambria Math", Font.PLAIN, 18));
+		
+		//Cria e adiciona o listener
+		ShowFigureBtnsHandler handler = new ShowFigureBtnsHandler();
+		buttonNext.addActionListener(handler);
+		buttonPrevious.addActionListener(handler);
+		btnSaveFigures.addActionListener(handler);
+		
+		//Cria o layout e adiciona os componentes
 		GroupLayout groupLayout = new GroupLayout(this);
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
@@ -64,20 +73,19 @@ public class PeriodFigureView extends JPanel {
 					.addComponent(buttonNext, GroupLayout.PREFERRED_SIZE, 112, GroupLayout.PREFERRED_SIZE)
 					.addGap(18)
 					.addComponent(buttonPrevious)
-					.addPreferredGap(ComponentPlacement.RELATED, 164, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED, 151, Short.MAX_VALUE)
 					.addComponent(btnSaveFigures)
-					.addContainerGap())
-				.addComponent(panelShowFigures, GroupLayout.DEFAULT_SIZE, 580, Short.MAX_VALUE)
+					.addGap(23))
+				.addComponent(panelShowFigures, GroupLayout.DEFAULT_SIZE, 550, Short.MAX_VALUE)
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.TRAILING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addComponent(panelShowFigures, GroupLayout.DEFAULT_SIZE, 385, Short.MAX_VALUE)
 					.addGap(13)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-							.addComponent(buttonNext, GroupLayout.PREFERRED_SIZE, 31, GroupLayout.PREFERRED_SIZE)
-							.addComponent(buttonPrevious))
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(buttonNext, GroupLayout.PREFERRED_SIZE, 31, GroupLayout.PREFERRED_SIZE)
+						.addComponent(buttonPrevious)
 						.addComponent(btnSaveFigures))
 					.addContainerGap())
 		);
