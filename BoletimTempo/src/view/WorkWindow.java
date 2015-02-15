59813d682e49bed8c31f13528393c892ff9e2c48
Patrick Lima <patrickm.lima1@gmail.com>
2015-02-15 14:55:46 -0400

    alterações para implementar o novo funcionamento (mudar de abas, etc.)

--- BoletimTempo/src/view/WorkWindow.java
+++ BoletimTempo/src/view/WorkWindow.java
@@ -1,39 +1,32 @@
 package view;
 
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JTabbedPane;
-import javax.swing.SwingUtilities;
+
+import model.util.ApplicationStatus;
 
 public class WorkWindow {
+	private ApplicationStatus status;
 	private static WorkWindow instance;
 	private JFrame frame;
 	private JTabbedPane tabbedPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					WorkWindow window = new WorkWindow();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the application.
 	 */
 	private WorkWindow() {
 		initialize();
+		status = ApplicationStatus.INITIALIZED;
+	}
+	
+	public void setStatus(ApplicationStatus status) {
+		this.status = status;
+	}
+	
+	public ApplicationStatus getStatus() {
+		return status;
 	}
 
 	public JTabbedPane getTabbedPane() {
@@ -46,6 +39,10 @@ public class WorkWindow {
 		}
 		return instance;
 	}
+	
+	public JFrame getFrame() {
+		return frame;
+	}
 
 	public void setNotClosable() {
 		this.frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
@@ -54,15 +51,15 @@ public class WorkWindow {
 	public void setClosable() {
 		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-
+	
 	public void setSelectedTab(int index) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				if (index < tabbedPane.getComponentCount()) {
-					tabbedPane.setSelectedIndex(index);
-				}
-			}
-		});
+		if(index < tabbedPane.getTabCount()) {
+			tabbedPane.setSelectedIndex(index);
+			tabbedPane.setEnabledAt(index, true);
+
+			configureTabbedPaneEnable();
+			tabbedPane.revalidate();
+		}
 	}
 
 	public void configureTabbedPaneEnable() {
@@ -70,6 +67,7 @@ public class WorkWindow {
 		switch (selected) {
 		case 0:
 			tabbedPane.setEnabledAt((selected++) + 1, false);
+			
 		case 1:
 			tabbedPane.setEnabledAt(selected + 1, false);
 			break;
@@ -101,13 +99,15 @@ public class WorkWindow {
 
 		WeatherDayView weatherDayView = new WeatherDayView();
 		tabbedPane
-				.addTab("Abrir Boletim", null, weatherDayView,
-						"Selecione para abrir e visualizar os dados do boletim meteorol\u00F3gico");
+				.addTab("Ver Boletim", null, weatherDayView,
+						"Selecione para visualizar os dados do boletim meteorol\u00F3gico");
 
 		PeriodFigureView figurePeriodView = new PeriodFigureView();
-		tabbedPane.addTab("Abrir Figura", null, figurePeriodView,
-				"Selecione para abrir as figuras de um dia meteorol\u00F3gico");
+		tabbedPane.addTab("Ver Figura dos turnos", null, figurePeriodView,
+				"Selecione para ver as figuras de um dia meteorol\u00F3gico");
 		frame.getContentPane().setLayout(groupLayout);
-//		configureTabbedPaneEnable();
+		configureTabbedPaneEnable();
+		
+		frame.setVisible(true);
 	}
 }
