4aac690d646b1b71ff8796a4769a81fa24b730e7
Patrick Lima <patrickm.lima1@gmail.com>
2015-02-17 01:04:58 -0400

    retirada dos botões de 'Próximo' e 'Anterior' e adição do botão 'Descartar'

--- BoletimTempo/src/view/PeriodFigureView.java
+++ BoletimTempo/src/view/PeriodFigureView.java
@@ -1,5 +1,7 @@
 package view;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
@@ -8,12 +10,24 @@ import java.awt.Font;
 
 import model.actionlistener.ShowFigureBtnsHandler;
 import model.componentchangelistener.PeriodFigureViewChanges;
+import model.util.ApplicationStatus;
+import model.util.ImageUtil;
 
 import javax.swing.JButton;
 import javax.swing.LayoutStyle.ComponentPlacement;
 
-import java.awt.Color;
+import controller.Controller;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ * JPanel which contains the necessary components to
+ * show the Period figures generated and save them
+ * @author Patrick M Lima
+ *
+ */
 public class PeriodFigureView extends JPanel {
 	/**
 	 * 
@@ -38,42 +52,38 @@ public class PeriodFigureView extends JPanel {
 		return panelShowFigures;
 	}
 	
+	/**
+	 * Initializes the components
+	 */
 	public void initialize() {
 		//Create the panel to show the figures
 		panelShowFigures = new JPanel();
 		panelShowFigures.setBackground(Color.GRAY);
 		
-		//Cria o JButton 'Anterior'
-		JButton buttonNext = new JButton("Anterior");
-		buttonNext.setToolTipText("Clique para ver as figuras do dia anterior");
-		buttonNext.setFont(new Font("Cambria Math", Font.PLAIN, 18));
-		
-		//Cria JButton 'Próximo'
-		JButton buttonPrevious = new JButton("Pr\u00F3ximo");
-		buttonPrevious.setToolTipText("Clique para ver as figuras do pr\u00F3ximo dia");
-		buttonPrevious.setFont(new Font("Cambria Math", Font.PLAIN, 18));
-		
 		//Cria o JButton 'Salvar figuras'
 		JButton btnSaveFigures = new JButton("Salvar figuras");
-		btnSaveFigures.setToolTipText("Clique aqui para salvar as figuras do turno");
+		btnSaveFigures.setToolTipText("Clique aqui para salvar as figuras exibidas");
 		btnSaveFigures.setFont(new Font("Cambria Math", Font.PLAIN, 18));
+		
+		//Cria o JButton 'Descartar'
+		JButton btnRuleOut = new JButton("Descartar");
+		btnRuleOut.setToolTipText("Clique aqui para descartar a figura exibida");
+		btnRuleOut.setFont(new Font("Cambria Math", Font.PLAIN, 18));
 		
 		//Cria e adiciona o listener
 		ShowFigureBtnsHandler handler = new ShowFigureBtnsHandler();
-		buttonNext.addActionListener(handler);
-		buttonPrevious.addActionListener(handler);
 		btnSaveFigures.addActionListener(handler);
+		btnRuleOut.addActionListener(handler);
+	
 		
 		//Cria o layout e adiciona os componentes
 		GroupLayout groupLayout = new GroupLayout(this);
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(buttonNext, GroupLayout.PREFERRED_SIZE, 112, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
-					.addComponent(buttonPrevious)
-					.addPreferredGap(ComponentPlacement.RELATED, 151, Short.MAX_VALUE)
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addContainerGap(277, Short.MAX_VALUE)
+					.addComponent(btnRuleOut)
+					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(btnSaveFigures)
 					.addGap(23))
 				.addComponent(panelShowFigures, GroupLayout.DEFAULT_SIZE, 550, Short.MAX_VALUE)
@@ -84,12 +94,40 @@ public class PeriodFigureView extends JPanel {
 					.addComponent(panelShowFigures, GroupLayout.DEFAULT_SIZE, 385, Short.MAX_VALUE)
 					.addGap(13)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(buttonNext, GroupLayout.PREFERRED_SIZE, 31, GroupLayout.PREFERRED_SIZE)
-						.addComponent(buttonPrevious)
-						.addComponent(btnSaveFigures))
+						.addComponent(btnSaveFigures)
+						.addComponent(btnRuleOut))
 					.addContainerGap())
 		);
 		setLayout(groupLayout);
 
+	}
+	
+	/**
+	 * Adds the period figures icons to be shown in a JPanel. There
+	 * the user can switch what figures will be save
+	 * @throws IOException case the controller instance can't be taken
+	 */
+	public void addFigures() throws IOException {
+		getPanelShowFigures().removeAll();
+		int w = getPanelShowFigures().getWidth()/4 -10;
+		int h = getPanelShowFigures().getHeight() -10;
+
+		Controller controller = null;
+		controller = Controller.getInstance();
+		
+		JLabel label = null;
+		BufferedImage[] imgs = controller.getImages();
+		if(imgs != null) {
+			for (BufferedImage bi : imgs) {
+				label = new JLabel();
+				label.setSize(w, h);
+				label.setIcon(new ImageIcon(ImageUtil.resize(bi, w, h)));
+				getPanelShowFigures().add(label);
+			}
+		} else
+			WorkWindow.getInstance().setStatus(
+					ApplicationStatus.PERIOD_FIGURES_SAVED);
+		revalidate();
+		repaint();
 	}
 }
