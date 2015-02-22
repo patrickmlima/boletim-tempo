d9cc49b7cf8a8bea05fc4ba535a2ffa9a81e23bf
Patrick Lima <patrickm.lima1@gmail.com>
2015-02-22 19:56:17 -0400

    nova organização de pacotes

---
+++ BoletimTempo/src/view/componentchangelistener/PeriodFigureViewChanges.java
@@ -0,0 +1,32 @@
+package view.componentchangelistener;
+
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.io.IOException;
+
+import view.PeriodFigureView;
+
+/**
+ * Implements methods to handle the change on component view
+ * 
+ * @author Patrick M Lima
+ *
+ */
+public class PeriodFigureViewChanges extends ComponentAdapter {
+	
+	public PeriodFigureViewChanges() {
+		super();
+	}
+	
+	@Override
+	public void componentShown(ComponentEvent component) {
+		PeriodFigureView pfView = (PeriodFigureView) component.getSource();
+		if (pfView.getPanelShowFigures().getComponentCount() == 0) {
+			try {
+				pfView.addFigures();
+			} catch (NullPointerException | IOException e) {
+				// e.printStackTrace();
+			}
+		}
+	}
+}
